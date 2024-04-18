3a2c119fe6e9f9be6e14d488e6bfbec8f94c5107
jkg2577249 <167415254+jkg2577249@users.noreply.github.com>
2024-04-18 18:55:34 +0900

    Update draw_fft_plot.py

--- draw_fft_plot.py
+++ draw_fft_plot.py
@@ -3,6 +3,7 @@ import scipy.io as sio
 import scipy.io.wavfile
 import matplotlib.pyplot as plt
 import argparse 
+from scipy import fftpack
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', required=False, default='filename.wav')
@@ -16,12 +17,14 @@ fftsize = len(data)
 data_fft = fftpack.fft(data, fftsize)
 
 Ts = 1/samplerate 
-############################################
-# WRITE DOWN YOUR FFT PLOT CODE HERE 
-# X axis is Frequency, Y axis is Power
 
+# Frequency axis
+freqs = np.fft.fftfreq(len(data_fft)) * samplerate
 
-
-############################################
-
+# Plot FFT
+plt.plot(freqs[:len(data_fft)//2], np.abs(data_fft[:len(data_fft)//2]))
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Power')
+plt.title('FFT of Audio Signal')
+plt.grid(True)
 plt.show()
