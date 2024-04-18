8230b1d4120a462e7c16df206ff2caa5856d6fa6
jkg2577249 <167415254+jkg2577249@users.noreply.github.com>
2024-04-18 18:59:08 +0900

    Update get_fft_time.py

--- get_fft_time.py
+++ get_fft_time.py
@@ -4,6 +4,7 @@ import scipy.io.wavfile
 import matplotlib.pyplot as plt
 import argparse 
 from scipy import fftpack
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', required=False, default='filename.wav')
@@ -12,7 +13,16 @@ args = parser.parse_args()
 print("drawing plot for", args.filename)
 
 samplerate, data = sio.wavfile.read(args.filename)
+
+# Start measuring time
+start_time = time.time()
+
 fftsize = len(data)
 data_fft = fftpack.fft(data, fftsize)
 
-# fast fourier transform 
+# End measuring time
+end_time = time.time()
+
+# Calculate elapsed time
+elapsed_time = end_time - start_time
+print("FFT execution time:", elapsed_time, "seconds")
